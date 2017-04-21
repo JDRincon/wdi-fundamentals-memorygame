854180e7b28cb2346ae365e33101c10e535f5237
JDRincon <JenniferD.Rincon@gmail.com>
2017-04-21 04:33:01 -0400

    Add files via upload

--- memory_game/js/secondary.js
+++ memory_game/js/secondary.js
@@ -1,8 +1,32 @@
-console.log("Up and running");
-var cards =["queen","queen","king","king"];
+var cards =[
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
 var cardsInPlay = [];
-cardsInPlay.push(cards[0]);
-cardsInPlay.push(cards[2]);
+cardsInPlay.push(cards[0].rank);
+	console.log(cards[0].cardImage);
+	console.log(cards[0].suit);
+cardsInPlay.push(cards[2].rank);
+	console.log(cards[2].cardImage);
+	console.log(cards[2].suit);
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!")
@@ -11,7 +35,7 @@ var checkForMatch = function() {
 	}
 }
 var flipcard = function(cardId){
-	console.log("User flipped" + " "+ cards[0] || "User flipped" + " " + cards[2])
+	console.log("User flipped" + " "+ cards[0].rank || "User flipped" + " " + cards[2].rank)
 if (cardsInPlay.length === 2){
 	checkForMatch();
 }
