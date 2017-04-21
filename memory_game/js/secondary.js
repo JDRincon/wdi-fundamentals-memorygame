f9d7796f94a150f20be3d0d1d4b9e6ccd20abecc
JDRincon <JenniferD.Rincon@gmail.com>
2017-04-21 04:12:07 -0400

    Add files via upload

--- memory_game/js/secondary.js
+++ memory_game/js/secondary.js
@@ -1,14 +1,23 @@
 console.log("Up and running");
 var cards =["queen","queen","king","king"];
 var cardsInPlay = [];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardThree');
-console.log("User flipped" + " " + cards[0]);
-console.log("User flipped" + " " + cards[2]);
-if (cardsInPlay.length === 2){
-	if (cards[0] === cards[1]){
+cardsInPlay.push(cards[0]);
+cardsInPlay.push(cards[2]);
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!")
 	} else {
 		alert("Sorry,try again")
 	}
 }
+var flipcard = function(cardId){
+	console.log("User flipped" + " "+ cards[0] || "User flipped" + " " + cards[2])
+if (cardsInPlay.length === 2){
+	checkForMatch();
+}
+else if (cardsInPlay.length < 2){
+	console.log("Sorry,try again")
+};
+}
+flipcard(0);
+flipcard(2);
